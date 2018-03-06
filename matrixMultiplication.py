38a64b2d825261509ab27211276b74be3170e2df
saify2j <30390844+saify2j@users.noreply.github.com>
2018-03-07 01:44:09 +0600

    2nd commit
    matrix multiplication without numpy in a userdefined function

--- matrixMultiplication.py
+++ matrixMultiplication.py
@@ -1,43 +1,52 @@
+def multiplymatrix(a, b):
+    res= []
+    for i in range(0, len(a)):
+        res.append([])
 
-import numpy as np
+    for i in range(0, len(a)):
+        for j in range(0, len(b[0])):
+            res[i].append(j)
+            res[i][j]= 0
+
+    for i in range(0, len(a)):
+        for j in range(0, len(b[0])):
+            for k in range(0, len(b)):
+                res[i][j]+= a[i][k]*b[k][j]
+
+    return res
+
+
+a= [[2,3],
+    [4,5],
+    [6,7]]
+b=[[1,3,5],
+   [4,5,6]]
+print(multiplymatrix(a,b))
+
+
 #
-# a=np.array([[2,3],[4,6]])
-# b=np.array([[2,3],[4,6]])
 #
-# c=np.array([[1,2,3,-3],
-#             [4,5,6,-2],
-#             [7,8,9,-1]])
+# a= [[1, 2],
+#     [2, 2],
+#     [3,3]]
+# b= [[1, 2],
+#     [2, 2]]
+# res=[]
+# #print(len(a), len(a[0]))
 #
-# #print(a.dot(b)) matrix multiplication
+# for i in range(0,len(a)):
+#     res.append([])
 #
-# #print(c[:3,1:3]) this helps to get a part of the matrix
+# for i in range(0, len(a)):
+#     for j in range(0, len(b[0])):
+#         res[i].append(j)
+#         res[i][j]= 0
 #
-# d=np.random.random((2,2))
-# print(d)
+# for i in range(0,len(a)):
+#     for j in range(0,len(b[0])):
+#
+#         for k in range(0,len(b)):
+#             res[i][j]+=a[i][k]*b[k][j]
+#
+# print(res)
 
-
-
-
-a= [[1, 2],
-    [2, 2],
-    [3,3]]
-b= [[1, 2],
-    [2, 2]]
-res=[]
-#print(len(a), len(a[0]))
-
-for i in range(0,len(a)):
-    res.append([])
-
-for i in range(0, len(a)):
-    for j in range(0, len(b[0])):
-        res[i].append(j)
-        res[i][j]= 0
-
-for i in range(0,len(a)):
-    for j in range(0,len(b[0])):
-
-        for k in range(0,len(b)):
-            res[i][j]+=a[i][k]*b[k][j]
-
-print(res)
